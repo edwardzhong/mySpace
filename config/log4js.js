dd2d2cdcd468057b9cf53be47403067d1aadadc2
edwardzhong <edwardloveyou@gmail.com>
2018-06-08 00:22:54 +0800

    update log4

--- config/log4js.js
+++ config/log4js.js
@@ -1,30 +1,26 @@
 /**
  * logger config
- * @type {Object}
  */
 module.exports={
-    "appenders": [
-        // {
-        //     "type": "console" 
-        // },
+    appenders: [
         {
-            "type": "dateFile",
-            "filename": "logs/",
-            "pattern": "yyyy-MM-dd_app.log",
-            "alwaysIncludePattern": true,
-            "maxLogSize": 1024,
-            "backups": 7,
-            "category": "systemInfo"
+            type: "dateFile",
+            filename: "logs/",
+            pattern: "yyyy-MM-dd_app.log",
+            alwaysIncludePattern: true,
+            maxLogSize: 1024,
+            backups: 7,
+            category: "systemInfo"
         },
         {
-            "type": "dateFile",
-            "filename": "logs/",
-            "pattern": "yyyy-MM-dd_conect.log",
-            "alwaysIncludePattern": true,
-            "maxLogSize": 2048,
-            "backups": 7,
-            "category": "accessInfo"
+            type: "dateFile",
+            filename: "logs/",
+            pattern: "yyyy-MM-dd_conect.log",
+            alwaysIncludePattern: true,
+            maxLogSize: 2048,
+            backups: 7,
+            category: "accessInfo"
         }
     ],
-    "replaceConsole": false
-}
+    replaceConsole: false
+};
