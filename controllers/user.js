9bd1435e4ac4fc79494b4bdbb31ef00dbfa9c9a0
edwardzhong <edwardloveyou@gmail.com>
2019-03-01 01:18:25 +0800

    update

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
-const userDao=require('../daos/user');
-const crypto=require('crypto');
-const log=require('../common/logger').logger();
+const userDao = require('../daos/user');
+const crypto = require('crypto');
+const log = require('../common/logger').logger();
 
 /**
  * user login
@@ -8,39 +8,32 @@ const log=require('../common/logger').logger();
  * @param  {Function} next [description]
  * @return {[type]}        [description]
  */
-exports.login=async function(ctx,next){
-	try{
-		let form=ctx.request.body;
-		let users= await userDao.query({email:form.email});
-		let ret={};
-		
-		if(users.length){
-			let user=users[0];
-			if(user.hash_password==encryptPass(form.password,user.salt)){
-				ret={
-					status:0,
-					msg:'登录成功'
-				};
-				ctx.session.user=user;
-			} else {
-				ret={
-					status:2,
-					msg:'密码错误'
-				};
-			}
+exports.login = async function (ctx, next) {
+	try {
+		let form = ctx.request.body;
+		let users = await userDao.query({ email: form.email });
+		let ret = { status: -1, msg: 'error' };
+
+		if (!users.length) {
+			ret.status = 1;
+			ret.msg = '用户不存在';
 		} else {
-			ret={
-				status:1,
-				msg:'用户不存在'
-			};
+			const user = users[0];
+			if (user.hash_password !== encryptPass(form.password, user.salt)) {
+				ret.status = 2;
+				ret.msg = '密码错误';
+			} else {
+				ret.status = 0;
+				ret.msg = '登录成功';
+			}
 		}
-		ctx.body=await ret;
-	} catch(err){
+		ctx.body = await ret;
+	} catch (err) {
 		log.error(err);
-		ctx.body=await{
-			status:-1,
-			error:err,
-			msg:'系统错误'
+		ctx.body = await {
+			status: -1,
+			error: err,
+			msg: '系统错误'
 		};
 	}
 };
@@ -51,25 +44,25 @@ exports.login=async function(ctx,next){
  * @param  {Function} next [description]
  * @return {[type]}        [description]
  */
-exports.register=async function(ctx,next){
-	let form=ctx.request.body;
-	try{
-		form.salt=makeSalt();
-		form.hash_password=encryptPass(form.password,form.salt);
+exports.register = async function (ctx, next) {
+	let form = ctx.request.body;
+	try {
+		form.salt = makeSalt();
+		form.hash_password = encryptPass(form.password, form.salt);
 		delete form.password;
-		
-		let result=await userDao.insert(form);
-		ctx.body=await {
-			status:0,
-			msg:'注册成功！',
-			result:result
+
+		let result = await userDao.insert(form);
+		ctx.body = await {
+			status: 0,
+			msg: '注册成功！',
+			result: result
 		}
-	} catch(err){
+	} catch (err) {
 		log.error(err);
-		ctx.body=await{
-			status:-1,
-			error:err,
-			msg:'系统错误'
+		ctx.body = await {
+			status: -1,
+			error: err,
+			msg: '系统错误'
 		};
 	}
 };
@@ -81,18 +74,18 @@ exports.register=async function(ctx,next){
  * @param  {Function} next [description]
  * @return {[type]}        [description]
  */
-exports.logout=async function(ctx,next){
-	let form=ctx.request.body;
-	ctx.session=null;
-	ctx.state=null;
-	ctx.body= await {
-		status:0,
-		msg:'注销成功'
+exports.logout = async function (ctx, next) {
+	let form = ctx.request.body;
+	ctx.session = null;
+	ctx.state = null;
+	ctx.body = await {
+		status: 0,
+		msg: '注销成功'
 	}
 };
 
 // generate	 salt/生成salt
-let makeSalt=()=>Math.round((new Date().valueOf() * Math.random())) + '';
+let makeSalt = () => Math.round((new Date().valueOf() * Math.random())) + '';
 
 // generate md5/生成md5
-let encryptPass=(pass,salt)=>crypto.createHash('md5').update(pass+salt).digest('hex');
+let encryptPass = (pass, salt) => crypto.createHash('md5').update(pass + salt).digest('hex');
